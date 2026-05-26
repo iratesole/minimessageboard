352ceb13f48cb172ead97f8770e6337d787db4ed
Arvind Bangarwa <arvindbangarwa15@gmail.com>
2026-05-26 23:42:44 +0530

    Some change in env to function better

--- db/populated.js
+++ db/populated.js
@@ -20,11 +20,7 @@ INSERT INTO messages (username, text)
 async function main() {
     console.log("seeding...");
     const client = new Client ({
-        user : process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        host : process.env.DB_HOST,
-        port : process.env.DB_PORT,
-        database : process.env.DB_DATABASE
+        connectionString : process.env.DATABASE_URL
     });
 
     await client.connect();
